7777fab47e7502c2c8d360d10f8253ea9b50566c
kangddong <rkdehddud96@naver.com>
2022-01-02 23:45:18 +0900

    Q1045 complete0

--- bojSubmit/main.swift
+++ bojSubmit/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+var input = readLine()!
+var inputArray = input.split(separator: " ")
 
+let a = Int(inputArray[0])!
+let b = Int(inputArray[1])!
+let c = Int(inputArray[2])!
+
+
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
