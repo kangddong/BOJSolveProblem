197a73c91d90f3388a20da1806751895f4728810
kangddong <rkdehddud96@naver.com>
2022-01-10 23:55:08 +0900

    2741

--- bojSubmit/for/Print1ton.swift
+++ bojSubmit/for/Print1ton.swift
@@ -1,5 +1,5 @@
 //
-//  2741번
+//  8393번
 //  bojSubmit
 //
 //  Created by 강동영 on 2022/01/10.
@@ -13,6 +13,7 @@ let num = Int(input)!
 var result = 0
 
 for i in 1...num {
-    print(i)
+    result += i
 }
+
 
