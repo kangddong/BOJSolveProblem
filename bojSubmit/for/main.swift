6128592100b81db19bc4b53a6d8bd7531b5dd596
kangddong <rkdehddud96@naver.com>
2022-01-10 23:50:57 +0900

    8393

---
+++ bojSubmit/for/main.swift
@@ -0,0 +1,20 @@
+//
+//  8393번
+//  bojSubmit
+//
+//  Created by 강동영 on 2022/01/10.
+//
+
+import Foundation
+
+let input = readLine()!
+let num = Int(input)!
+
+var result = 0
+
+for i in 1...num {
+    result += i
+}
+
+print(result)
+
