b946da4fd7259dd51cdefc0c0a8e211b7b52c84e
kangddong <rkdehddud96@naver.com>
2022-01-17 23:15:55 +0900

    star2439

---
+++ bojSubmit/for/PrintStar.swift
@@ -0,0 +1,16 @@
+//
+//  2438번 문제
+//  bojSubmit
+//
+//  Created by 강동영 on 2022/01/17.
+//
+
+import Foundation
+
+let input = Int(readLine()!)!
+var star:String = ""
+
+for _ in 1...input {
+    star += "*"
+    print(star)
+}
