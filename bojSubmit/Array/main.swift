d550c9d5997c608f3bfc8523801d865f2ebce519
kangddong <rkdehddud96@naver.com>
2022-02-15 22:35:27 +0900

    2577

--- bojSubmit/Array/main.swift
+++ bojSubmit/Array/main.swift
@@ -1,26 +1,38 @@
 //
-//  2562.swift
+//  2577.swift
 //  bojSubmit
 //
-//  Created by 강동영 on 2022/02/13.
+//  Created by 강동영 on 2022/02/15.
 //
 
 import Foundation
-var numberArray: Array<Int> = []
 
-for _ in 0...8 {
-    let input = Int(readLine()!)!
-    numberArray.append(input)
+var input: Int
+var sum = 1
+let numberArray: Array<String> = ["0","1","2","3","4","5","6","7","8","9"]
+var count = 0
+
+for _ in 0...2 {
+    input = Int(readLine()!)!
+    sum *= input
 }
 
-let numberMax = numberArray.max()!
-print(numberMax)
+let inputedString = sum.description
 
-for (index, item) in numberArray.enumerated() {
-    if numberMax == item {
-        print(index+1)
+
+
+numberArray.forEach {
+    count = 0
+    
+    for index in inputedString {
+        if String(index) == $0 {
+            count += 1
+        }
     }
+    print(count)
 }
+
+
 
 
 
